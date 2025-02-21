b23d56ffcd335d92b4d8b7bff52f674ffbc4b236
StefanoRaimondi-prog <stefanoraimondi.pl@gmail.com>
2025-02-21 17:00:01 +0100

    Create Stefano.java
    creazione funzione registrazione

---
+++ Stefano.java
@@ -0,0 +1,15 @@
+
+}
+
+static void registrazione() {
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine(); // Legge il nome dell'utente
+        
+        System.out.print("Password: ");
+        String password = scanner.nextLine(); // Legge la password
+        
+        System.out.print("Età: ");
+        int eta = leggiIntero(); // Legge l'età dell'utente
+        
+        System.out.print("Email: ");
+        String email = scanner.nextLine(); // Legge l'email dell'utente
