09342934a6ec5c0f064317fc594704867c10695f
Antonio Russomando <tonyrussomando@gmail.com>
2025-02-21 16:30:31 +0100

    Update Antonio.java

--- Antonio.java
+++ Antonio.java
@@ -15,51 +15,80 @@ public class Antonio {
 
         while (true) {
             System.out.println(
-                    "Scegli un'operazione (1 - Addizione, 2 - Sottrazione, 3 - Moltiplicazione, 4 - Divisione, -1=Esci): ");
+                    "Vuoi effettuare un'operazione? (1 - Addizione, 2 - Sottrazione, 3 - Moltiplicazione, 4 - Divisione, -1=Esci): ");
             double scelta = input.nextDouble();
 
             if (scelta == -1) {
                 System.out.println("Uscita dalla calcolatrice");
                 break;
             } else if (scelta == 1) {
+                // Prendo gli operandi dell'operazione
                 System.out.println("Aggiungi addendo1: ");
                 double addendo1 = input.nextDouble();
+                input.next();
+
                 System.out.println("Aggiungi addendo2: ");
                 double addendo2 = input.nextDouble();
+                input.next();
+
                 operazione = addendo1 + addendo2;
                 System.out.println("Risultato addizione: " + operazione);
+
+                // Aggiunta risultato all'arraylist
                 calcoliEffettuali.add(operazione);
 
             } else if (scelta == 2) {
+                // Prendo gli operandi dell'operazione
                 System.out.println("Aggiungi sottraendo1: ");
                 double sottraendo1 = input.nextDouble();
+                input.next();
+
                 System.out.println("Aggiungi sottraendo2: ");
                 double sottraendo2 = input.nextDouble();
+                input.next();
+
                 operazione = sottraendo1 - sottraendo2;
                 System.out.println("Risultato sottraendo: " + operazione);
+
+                // Aggiunta risultato all'arraylist
                 calcoliEffettuali.add(operazione);
 
             } else if (scelta == 3) {
+                // Prendo gli operandi dell'operazione
                 System.out.println("Aggiungi fattore1: ");
                 double fattore1 = input.nextDouble();
+                input.next();
+
                 System.out.println("Aggiungi fattore2: ");
                 double fattore2 = input.nextDouble();
+                input.next();
+
                 operazione = fattore1 * fattore2;
                 System.out.println("Risultato moltiplicazione: " + operazione);
+
+                // Aggiunta risultato all'arraylist
                 calcoliEffettuali.add(operazione);
 
             } else if (scelta == 4) {
+                // Prendo gli operandi dell'operazione
                 System.out.println("Aggiungi dividendo: ");
                 double dividendo = input.nextDouble();
+                input.next();
+
                 System.out.println("Aggiungi divisore: ");
                 double divisore = input.nextDouble();
+                input.next();
 
-                while (divisore == 0) { // Evita la divisione per zero
+                // Evita la divisione per zero
+                while (divisore == 0) {
                     System.out.println("Errore: Il divisore non può essere zero! Inserisci un altro numero:");
                     divisore = input.nextDouble();
+                    input.next();
                 }
                 operazione = dividendo / divisore;
                 System.out.println("Risultato divisione: " + operazione);
+
+                // Aggiunta risultato all'arraylist
                 calcoliEffettuali.add(operazione);
 
             } else {
@@ -70,12 +99,13 @@ public class Antonio {
         input.close();
     }
 
+    // Funzione per visualizzare tutti i calcoli precedenti
     public static void inventario(ArrayList<Double> calcoliEffettuati) {
         if (calcoliEffettuati.isEmpty()) {
             System.out.println("Nessuna operazione salvata!");
             return;
         }
-
+        // Ciclo per la stampa dei calcoli effettuati in precedenza
         for (int i = 1; i <= calcoliEffettuati.size(); i++) {
             System.out.println("Operazione " + i + " -> Risultato: " + calcoliEffettuati.get(i));
         }
